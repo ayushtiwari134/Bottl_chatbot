4695191b1e6a36e32e699fbd980777b89a9a3d06
ayushtiwari134 <ayushtiwari134@gmail.com>
2024-01-17 21:33:24 +0530

    new file:   __pycache__/app.cpython-311.pyc 	modified:   app.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -1,47 +1,60 @@
-from flask import Flask,request
-import requests
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-import requests
-import qrcode
+from twilio import *
+
+
 app = Flask(__name__)
+count=0
+from twilio.rest import Client
+
+account_sid = 'AC68dece1aeedd5877f9b238a3cdfca6d0'
+auth_token = '034927186136026d394e4cb36cdacbef'
+client = Client(account_sid, auth_token)
+
+# message = client.messages.create(
+#   from_='whatsapp:+14155238886',
+#   body='',
+#   to='whatsapp:+917439042931'
+# )
+
+# print(message.sid)
+
 @app.route("/sms", methods=['POST'])
 def reply():
+    
     incoming_msg = request.form.get('Body').lower()
     response = MessagingResponse()
-   
     print(incoming_msg)
-
     message=response.message()
-    responded=False
+    responded = False
     words = incoming_msg.split('@')
     if "hello" in incoming_msg:
-        reply = "Welcome to BMTC ticket bot. "
-
-        reply = "Hello! \n I am your ticket-bot and I will assist you in purchasing bus tickets, bus passes and tracking live location of busses \n Type your queries or choose one of the options below for me to assist you  "
+        reply = "Welcome to BMTC ticket bot"
+        reply = "Hello! I am your ticket-bot and I will assist you in purchasing bus tickets, bus passes and tracking live location of busses. Type your queries or choose one of the options below for me to assist you"
         message.body(reply)
-        responded=True
-
+        responded = True
         
-
         
-    if len(words)== 2 and "Bus Tickets" in incoming_msg:
-        reminder_string="Would you like to enter your bus stop or bus number "
+    if len(words) == 2 and "Bus tickets" in incoming_msg:
+        reminder_string = "Would you like to enter your bus stop or bus number"
         message.body(reminder_string)
         responded = True
-        if len(words)== 2 and "Bus stop" in incoming_msg:
-            reminder_string="Please enter the source stop name choose one of the following options or type your query"
-            message.body(reminder_string)
 
-            if len(words)== 3 and "type stop name" in incoming_msg:
-                reminder_string="Please type your stop name now"
+        if len(words)==2 and "Bus stop" in incoming_msg:
+            reminder_string = "Please enter the source stop name choose one of the following options or type your query"
+            message.body(reminder_string)
+            responded = True
+            
+            if len(words)==3 and "Type stop name" in incoming_msg:
+                reminder_string = "Please type your stop name"
                 message.body(reminder_string)
+                responded = True
 
-            responded = True
-        elif len(words)== 2 and "Bus number" in incoming_msg:
-            reminder_string="Enter the bus number"
+
+        elif len(words)==2 and "Bus Number" in incoming_msg:
+            reminder_string="Please enter your bus number"
             message.body(reminder_string)
-            responded = True
+            responded= True 
         
-
 if __name__ == "__main__":
     app.run(debug=True)
